6d8980cb548ee442229bd0b79168588e37b000e7
Anwarsyah <62179765+nwarsyh@users.noreply.github.com>
2026-05-24 17:57:20 +0700

    Update saw_config.php

--- saw_app/saw_config/saw_config.php
+++ saw_app/saw_config/saw_config.php
@@ -1,5 +1,5 @@
 <?php
-define('BASEURL', 'http://localhost/githubanwar/spk_saw_php_mvc');
+define('BASEURL', 'http://localhost/githubanwar/spk_saw_php_mvc_v1');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
